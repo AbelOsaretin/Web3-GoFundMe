aea7ab58c2e2c579bd3a70e833cf96e15ad6b82e
Abel Osaretin <contact.abel321@gmail.com>
2025-11-22 07:10:08 +0100

    Project Init

---
+++ Contract/script/contract.s.sol
@@ -0,0 +1,18 @@
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.13;
+
+// import "forge-std/Script.sol";
+
+// import {CrowdFund} from "../src//CrowdFund.sol";
+
+// contract ContractScript is Script {
+//     function run() external {
+//         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+//         vm.startBroadcast(deployerPrivateKey);
+
+//         CrowdFund _contract = new CrowdFund("ARG1", "ARG2", 2000);
+
+//         vm.stopBroadcast();
+//     }
+// }
+
